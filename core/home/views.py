775a5d662b0ac77034f56df3c18eac7cc2e5d7c9
Mahadi <smmahadi01910048684@gmail.com>
2024-11-11 17:50:19 +0600

    v

--- core/home/views.py
+++ core/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# def home(request):
+#     return HttpResponse("Hey I am a Django Server.")
+def success_page(request):
+    return HttpResponse("<h1>Hey this is a Success page</h1>")
+
+
 def home(request):
-    return HttpResponse("Hey I am a Django Server.")
+    return render(request, "home/index.html")
